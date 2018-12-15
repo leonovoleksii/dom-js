330c590f57583eaad215249b3b7146306e394eb3
leonovoleksii <leonov.oleksii73@gmail.com>
2018-12-15 15:27:30 +0200

    add functionality for img

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
 const content = [
     {
         h1: 'What is Lorem Ipsum?',
-        p: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the' +
+        p: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the' +
             'industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type' +
             'and scrambled it to make a type specimen book. It has survived not only five centuries, but also the' +
             'leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s' +
             'with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop' +
-            'publishing software like Aldus PageMaker including versions of Lorem Ipsum.']
+            'publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        img: 'img/1.png'
     },
     {
         h1: 'Why do we use it?',
-        p: ['It is a long established fact that a reader will be distracted by the readable content of a page when' +
+        p: 'It is a long established fact that a reader will be distracted by the readable content of a page when' +
             ' looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution' +
             ' of letters, as opposed to using \'Content here, content here\', making it look like readable English.' +
             ' Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,' +
             ' and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have' +
-            ' evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
-        ]
+            ' evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+        img: 'img/1.png'
     },
     {
         h1: 'Where does it come from?',
-        p: [ 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical ' +
+        p: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical ' +
             'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at ' +
             'Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from ' +
             'a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered ' +
@@ -28,36 +29,48 @@ const content = [
             'et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on ' +
             'the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum ' +
             'dolor sit amet..", comes from a line in section 1.10.32.',
-
-            'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. ' +
-            'Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in ' +
-            'their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'
-        ]
+        img: 'img/1.png'
     },
     {
         h1: 'Where can I get some?',
-        p:  ['but the majority have suffered alteration in some form, by injected humour, or randomised words which ' +
+        p:  'but the majority have suffered alteration in some form, by injected humour, or randomised words which ' +
             'don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be ' +
             'sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on ' +
             'the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the ' +
             'Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence ' +
             'structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore ' +
-            'always free from repetition, injected humour, or non-characteristic words etc.'
-        ]
+            'always free from repetition, injected humour, or non-characteristic words etc.',
+        img: 'img/1.png'
     }
 ];
 
 let fillTheContent = function() {
     const divs = document.querySelectorAll('.content');
     for (let i = 0; i < divs.length; i++) {
+
         let div = divs[i];
         let elem;
+
+        let createTextElem = function(elem) {
+            let element = document.createElement(elem);
+            let node = document.createTextNode(content[i][elem]);
+            element.appendChild(node);
+            div.appendChild(element);
+        };
+
+        let createImgElem = function(elem) {
+            let element = document.createElement(elem);
+            element.setAttribute('src', content[i][elem]);
+            div.appendChild(element);
+        };
+
         for (elem in content[i]) {
             if (content[i].hasOwnProperty(elem)) {
-                let element = document.createElement(elem);
-                let node = document.createTextNode(content[i][elem]);
-                element.appendChild(node);
-                div.appendChild(element);
+                if (elem !== 'img') {
+                    createTextElem(elem);
+                } else {
+                    createImgElem(elem);
+                }
             }
         }
     }
