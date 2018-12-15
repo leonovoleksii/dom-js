082a420392bed1dd8e79c5fd5c7065716eb0d6ca
leonovoleksii <leonov.oleksii73@gmail.com>
2018-12-15 16:05:34 +0200

    change the content

--- js/app.js
+++ js/app.js
@@ -1,46 +1,34 @@
 const content = [
     {
-        h1: 'What is Lorem Ipsum?',
-        p: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the' +
-            'industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type' +
-            'and scrambled it to make a type specimen book. It has survived not only five centuries, but also the' +
-            'leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s' +
-            'with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop' +
-            'publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        img: 'img/1.png'
+        h1: 'HTML',
+        img: 'img/html.png',
+        p: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web ' +
+            'applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone ' +
+            'technologies for the World Wide Web.'
     },
     {
-        h1: 'Why do we use it?',
-        p: 'It is a long established fact that a reader will be distracted by the readable content of a page when' +
-            ' looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution' +
-            ' of letters, as opposed to using \'Content here, content here\', making it look like readable English.' +
-            ' Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,' +
-            ' and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have' +
-            ' evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-        img: 'img/1.png'
+        h1: 'CSS',
+        img: 'img/css.png',
+        p: 'CSS is designed to enable the separation of presentation and content, including layout, colors, and fonts.' +
+            ' This separation can improve content accessibility, provide more flexibility and control in the ' +
+            'specification of presentation characteristics, enable multiple web pages to share formatting by specifying' +
+            ' the relevant CSS in a separate .css file, and reduce complexity and repetition in the structural content.'
     },
     {
-        h1: 'Where does it come from?',
-        p: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical ' +
-            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at ' +
-            'Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from ' +
-            'a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered ' +
-            'the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum ' +
-            'et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on ' +
-            'the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum ' +
-            'dolor sit amet..", comes from a line in section 1.10.32.',
-        img: 'img/1.png'
+        h1: 'JavaScript',
+        img: 'img/JavaScript.png',
+        p: 'JavaScript (JS) is a lightweight, interpreted or JIT compiled programming language with first-class ' +
+            'functions. Most well-known as the scripting language for Web pages, many non-browser environments also use' +
+            ' it, such as node.js and Apache CouchDB. JS is a prototype-based, multi-paradigm, dynamic scripting' +
+            ' language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.'
+
     },
     {
-        h1: 'Where can I get some?',
-        p:  'but the majority have suffered alteration in some form, by injected humour, or randomised words which ' +
-            'don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be ' +
-            'sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on ' +
-            'the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the ' +
-            'Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence ' +
-            'structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore ' +
-            'always free from repetition, injected humour, or non-characteristic words etc.',
-        img: 'img/1.png'
+        h1: 'DOM',
+        img: 'img/dom.png',
+        p:  'The Document Object Model (DOM) is a programming interface for HTML and XML documents. It represents the' +
+            ' page so that programs can change the document structure, style, and content. The DOM represents the' +
+            ' document as nodes and objects.'
     }
 ];
 
